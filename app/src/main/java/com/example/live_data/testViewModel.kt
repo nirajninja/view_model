4b93b58632f3ab916aceba1079377d0a54347582
nirajninja <nirajgusain5@gmail.com>
2021-03-16 11:44:33 +0530

    lol

---
+++ app/src/main/java/com/example/live_data/testViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.live_data
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TestViewModel:ViewModel(){
+    var number=0
+    val currentNumber:MutableLiveData<Int> by lazy {
+        MutableLiveData<Int>()
+    }
+    val currentBoolean:MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>()
+
+    }
+
+
+}
